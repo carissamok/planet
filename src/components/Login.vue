019a4619ce4754374f0f873b52de4acfa6e2b6b2
carissamok <61439305+carissamok@users.noreply.github.com>
2021-05-25 20:59:34 -0400

    added in login vue file

---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="login">
+        <img alt="Calendar logo" src="../assets/calendar.png">
+        <button v-on:click="login">Log in</button>
+        <button v-on:click="google">Sign up with Google</button>
+    </div>
+
+</template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api'
+    export default {
+        name:"Login",
+        methods: {
+            login() {
+                console.log("login was pressed")
+            },
+            google() {
+                console.log("google was pressed")
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
