97e860bb6126c174b63928ce9e43faf558d42e18
Chris He <bobbyslug@gmail.com>
2021-05-30 19:54:33 -0400

    database testing

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+
+function test() {
+  return firebase
+    .firestore()
+    .collection("test")
+    .add({
+      text: "hello this is me testing out the database",
+    })
+    .catch(function(error) {
+      console.error("Error writing test message to database", error);
+    });
+}
+test();
