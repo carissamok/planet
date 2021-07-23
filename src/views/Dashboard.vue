f265ac8eba903a26bcf51bdd0819fd3ea53075bb
juliewang8 <julieewang822@gmail.com>
2021-07-22 21:08:43 -0400

    merge pt2

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -10,15 +10,11 @@
 import SubNavbar from "../components/SubNavbar.vue";
 import Navbar from "../components/Navbar.vue";
 import CalendarMonth from "../components/calendar/CalendarMonth.vue";
-<<<<<<< HEAD
-import { createEvent } from "../firebase/events.js";
-=======
 import addGCalEvents from "../firebase/googleCalendar.js";
 import { upcomingEvents } from "../firebase/events.js";
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
->>>>>>> master
 
 export default {
   components: {
@@ -28,13 +24,6 @@ export default {
   },
   name: "Dashboard",
   beforeMount() {
-<<<<<<< HEAD
-    // addGCalEvents();
-    // addPlanetCalendar();
-    // createGcalEvent()
-
-    createEvent("julieewang822@gmail.com", "test", [{"email": "julie.wang@toasttab.com"}], "description", "2021-07-28T8:00:00-04:00", "2021-07-28T9:00:00-04:00", "location")
-=======
     firebase.auth().onAuthStateChanged(function (user) {
       if (user) {
         console.log("logged in");
@@ -43,7 +32,6 @@ export default {
         upcomingEvents().then((events) => console.log(events[0]));
       }
     });
->>>>>>> master
   },
 };
 </script>
